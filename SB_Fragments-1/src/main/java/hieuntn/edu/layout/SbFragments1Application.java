ce998cc8fe5ec1c425e06c3ec1fe9bbe340da4bd
hieuntn <hieuzz0505@gmail.com>
2025-03-24 01:11:33 +0700

    bài tập về nhà

---
+++ SB_Fragments-1/src/main/java/hieuntn/edu/layout/SbFragments1Application.java
@@ -0,0 +1,13 @@
+package hieuntn.edu.layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFragments1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFragments1Application.class, args);
+	}
+
+}
