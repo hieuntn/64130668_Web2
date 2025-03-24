292e790faf4de803cce13c55f71f25d5d592891e
hieuntn <hieuzz0505@gmail.com>
2025-03-24 09:36:10 +0700

    bài tập về nhà

---
+++ SB_Fragments-1/src/test/java/hieuntn/edu/layout/SbFragments1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hieuntn.edu.layout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFragments1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
