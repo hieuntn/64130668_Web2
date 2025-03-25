fd06d8768bc7dea3e8c69a615ccb694535e0ef1d
hieuntn <hieuzz0505@gmail.com>
2025-03-25 09:26:03 +0700

    puch lần đầu

---
+++ TongHopGiuaKi/src/test/java/hieuntn/edu/kt/TongHopGiuaKiApplicationTests.java
@@ -0,0 +1,13 @@
+package hieuntn.edu.kt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGiuaKiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
