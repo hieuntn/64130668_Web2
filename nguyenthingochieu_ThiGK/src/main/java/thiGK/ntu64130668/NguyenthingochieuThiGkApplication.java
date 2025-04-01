c11e9d26a586a6b29a27063625601b9eedf1a2de
hieuntn <hieuzz0505@gmail.com>
2025-04-01 09:04:20 +0700

    Kiem tra giua ki

---
+++ nguyenthingochieu_ThiGK/src/main/java/thiGK/ntu64130668/NguyenthingochieuThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130668;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenthingochieuThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenthingochieuThiGkApplication.class, args);
+	}
+
+}
